d50d6354d7d046bac9477dcc387422831736d447
A-N-I-T-T-A <anelizabeth62@gmail.com>
2024-01-04 17:50:20 +0530

    re commit

--- training_management/settings.py
+++ training_management/settings.py
@@ -138,5 +138,5 @@ AUTH_USER_MODEL = 'trainingapp.CustomUser'
 EMAIL_HOST='smtp.gmail.com'
 EMAIL_USE_TLS=True
 EMAIL_PORT=587
-EMAIL_HOST_USER='abcd@gmail.com'
-EMAIL_HOST_PASSWORD=''
+EMAIL_HOST_USER='anitta6238@gmail.com'
+EMAIL_HOST_PASSWORD='aiylpadpoeqdeiqq'
